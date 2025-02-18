ebde5412719b6cee80a13c1fedc5fbbcf746ea36
fagoon1311 <fs14smailbox65@gmail.com>
2025-02-18 20:47:23 +0530

    landing page, routes and api setup

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/ui/header";
+import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -13,7 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme:dark
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
@@ -32,7 +35,7 @@ export default function RootLayout({ children }) {
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made With Love by Fagoon.</p>
+                <p>Made With 💗 by Fagoon.</p>
               </div>
             </footer>
           </ThemeProvider>
--- components/header.jsx
+++ components/header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
 
         <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
-          <Link href="/dashboard">
+            <Link href={"/dashboard"}>
               <Button
                 variant="outline"
                 className="hidden md:inline-flex items-center gap-2"
@@ -48,57 +48,57 @@ const Header = () => {
                 <LayoutDashboard className="h-4 w-4" />
               </Button>
             </Link>
-          {/* Growth Tools */}
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button>
-                <StarsIcon className="h-4 w-4" />
-                <span className="hidden md:block">Growth Tools</span>
-                <ChevronDown className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href="/resume" className="flex items-center gap-2">
-                  <FileText className="h-4 w-4" />
-                  Build Resume
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link
-                  href="/ai-cover-letter"
-                  className="flex items-center gap-2"
+            {/* Growth Tools */}
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button>
+                  <StarsIcon className="h-4 w-4" />
+                  <span className="hidden md:block">Growth Tools</span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={"/resume"} className="flex items-center gap-2">
+                    <FileText className="h-4 w-4" />
+                    Build Resume
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/ai-cover-letter"}
+                    className="flex items-center gap-2"
                   >
-                  <PenBox className="h-4 w-4" />
-                  Cover Letter
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link href="/interview" className="flex items-center gap-2">
-                  <GraduationCap className="h-4 w-4" />
-                  Interview Prep
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                    <PenBox className="h-4 w-4" />
+                    Cover Letter
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={"/interview"} className="flex items-center gap-2">
+                    <GraduationCap className="h-4 w-4" />
+                    Interview Prep
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
 
           <SignedOut>
             <SignInButton>
-              <Button variant='outline'>Sign In</Button>
+              <Button variant="outline">Sign In</Button>
             </SignInButton>
           </SignedOut>
 
-
           <SignedIn>
-            <UserButton appearance={{
-              elements:{
-                avatarBox: 'w-10 h-10',
-                userButtonPopoverCard: 'shadow-xl',
-                userPreviewMainIdentifier: 'font-semibold',
-              }
-            }}
-            afterSignOutUrl="/"
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
             />
           </SignedIn>
         </div>
